0308ba7d90e2555f895d0a8fe4c99fdce49e37c7
giangbimin <gianghoang.dev@gmail.com>
2023-10-24 01:44:49 +0700

    refactor: add new api

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,69 @@
-const theme = require('tailwindcss/defaultTheme')
 const formPlugin = require('@tailwindcss/forms')
 const animatePlugin = require('tailwindcss-animate')
+const flowBite = require('flowbite/plugin')
 
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './public/**/*.html',
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {
-      fontFamily: {
-        ...theme.fontFamily,
-        sans: ['Inter', ...theme.fontFamily.serif],
-      },
       colors: {
-        pink: {
-          500: '#E4526E',
-          600: '#E13F5E',
-          700: '#CA3854',
+        primary: {
+          50: '#eff6ff',
+          100: '#dbeafe',
+          200: '#bfdbfe',
+          300: '#93c5fd',
+          400: '#60a5fa',
+          500: '#3b82f6',
+          600: '#2563eb',
+          700: '#1d4ed8',
+          800: '#1e40af',
+          900: '#1e3a8a',
+          950: '#172554',
         },
+      },
+      fontFamily: {
+        body: [
+          'Inter',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'system-ui',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+        ],
+        sans: [
+          'Inter',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'system-ui',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+        ],
       },
     },
   },
-  plugins: [formPlugin, animatePlugin],
+  plugins: [formPlugin, animatePlugin, flowBite],
 }
